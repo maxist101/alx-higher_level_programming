0f2677d698e9506761dadcd1a2e176fab5bbc393
maxist101 <netochukwu26@gamil.com>
2023-12-19 00:58:39 -0800

    0x06. Python - Classes and Objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square module"""
+
+
+class Square:
+    """square definition."""
+    def __init__(self, size):
+        """size: square length"""
+        self.__size = size
