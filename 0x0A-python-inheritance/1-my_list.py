598edf57f4ffd0a239921610fa5fa0eaa4c19b4a
maxist101 <netochukwu26@gamil.com>
2024-01-08 07:39:46 -0800

    0x0A. Python - Inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""class mylist"""
+"""MyList that inherits from list"""
 
 
 class MyList(list):
-    """prints sorted list of class"""
+    """contains class"""
+
     def print_sorted(self):
+        """Print self sorted list"""
         print(sorted(self))
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Module implementing BaseGeometry class"""
+"""base geometry"""
 
 
 class BaseGeometry:
-    """BaseGeometry class to extend"""
+    """a class BaseGeometry"""
+
     def area(self):
-        """Function to compute area of self"""
-        raise Exception('area() is not implemented')
+        """calculate area"""
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Function to validate value is an int"""
-        if type(value) is not int:
-            raise TypeError('{} must be an integer'.format(name))
+        """confirms a parameter as an int"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
+            raise ValueError("{} must be greater than 0".format(name))
