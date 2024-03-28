84841ce2a27bb75c32211fb06329f769fafde409
Enetreal <wizzarddguru@gmail.com>
2024-03-28 05:28:50 -0700

    done

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -13,4 +13,3 @@ if __name__ == "__main__":
         print("\t- type: {}".format(type(info)))
         print("\t- content: {}".format(info))
         print("\t- utf8 content: {}".format(info.decode('utf-8')))
-
